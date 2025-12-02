d0a7aeef7b97f41b550277d56c8ad8bb8c23bc22
GOPAL-YADAV-D <gopalyadav.d.888@gmail.com>
2025-12-02 22:20:20 +0530

    Learn and Understand: Body Parser Package and How To use the Mehod Specific Middleware in Express

--- ExpressBasics/app.js
+++ ExpressBasics/app.js
@@ -1,19 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log("This Always Runs!");
-    next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/add_product',(req, res, next) => {
-    console.log('Staring the First Middleware');
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
 app.use('/',(req, res, next) => {
-    console.log('In the Second Middleware');
     res.send('<h1>Hello from Express.js Server!</h1>');
 }); 
 

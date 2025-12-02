a10d4f6720e395d953f3090fc153cd4f300eb624
GOPAL-YADAV-D <gopalyadav.d.888@gmail.com>
2025-12-02 21:41:45 +0530

    Learn: Express Routing in use() util

--- ExpressBasics/app.js
+++ ExpressBasics/app.js
@@ -2,12 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Staring the First Middleware');
+app.use('/', (req, res, next) => {
+    console.log("This Always Runs!");
     next();
 });
 
-app.use((req, res, next) => {
+app.use('/add_product',(req, res, next) => {
+    console.log('Staring the First Middleware');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.use('/',(req, res, next) => {
     console.log('In the Second Middleware');
     res.send('<h1>Hello from Express.js Server!</h1>');
 }); 

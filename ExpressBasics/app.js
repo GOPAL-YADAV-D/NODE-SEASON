b54c4d7667204f98c9e1298993a2dd7630700930
GOPAL-YADAV-D <gopalyadav.d.888@gmail.com>
2025-12-03 14:14:18 +0530

    Refactor: Views
    Update Styles
    
    Learn and Set-up Handlebars templating engine

--- ExpressBasics/app.js
+++ ExpressBasics/app.js
@@ -2,17 +2,22 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHandlebars = require('express-handlebars');
 
 const rootDir = require('./util/rootPath');
-const adminRouter = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
 const app = express();
 
+app.engine('handlebars', expressHandlebars());
+app.set('view engine', 'handlebars');
+app.set('views', 'views');
+
 app.use(express.static(path.join(rootDir, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/admin', adminRouter);
+app.use('/admin', adminData.routes);
 app.use(shopRouter);
 
 app.use((req, res, next) => {

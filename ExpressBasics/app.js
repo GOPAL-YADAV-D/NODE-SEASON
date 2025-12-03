b76c5354080cfcee5ae3aa346b1dbe1fcbe9953e
GOPAL-YADAV-D <gopalyadav.d.888@gmail.com>
2025-12-03 12:36:13 +0530

    Learn: Routing Logic and Using Utility to get RootDirectory
    Understand Views
    
    Understand Static File Serving

--- ExpressBasics/app.js
+++ ExpressBasics/app.js
@@ -1,18 +1,22 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const rootDir = require('./util/rootPath');
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
 const app = express();
 
+app.use(express.static(path.join(rootDir, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/admin', adminRouter);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1> Page Not Found </h1>');
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 })
 
 app.listen(3000);
